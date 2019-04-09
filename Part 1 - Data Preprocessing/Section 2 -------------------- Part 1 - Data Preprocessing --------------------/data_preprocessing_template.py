0d5c2481f8c84f2193d164f1d32f3c2f1c0f48be
Anchia Villegas <y.anchia.villegas@accenture.com>
2019-04-09 13:12:22 -0600

    update the python template

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -7,17 +7,34 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values # the independent variable(s)
+y = dataset.iloc[:, 3].values # the dependent variable
+
+
+# encode categorical data --- DELETE IF UNNEEDED ----------------------------------------------
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+column_to_encode = ____Column__Number___
+X[:, column_to_encode] = LabelEncoder().fit_transform(X[:, column_to_encode]) # encode the last colum (Countries)
+# --- --- categorical_features : array of columns considered
+X = OneHotEncoder(categorical_features = [column_to_encode]).fit_transform(X).toarray()
+# -----------------------------------------------------------------------------
+
+
+# avoid the dummy variable trap --- DELETE IF UNNEEDED ----------------------------------------
+X = X[:, 1:] # remove the fist column
+
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+# Feature Scaling, 
+# --- not needed for simple and multiple linear regressions algorithms on R and Python
+"""
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
+"""
