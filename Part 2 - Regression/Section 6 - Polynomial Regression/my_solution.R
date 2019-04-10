28d18995b4aab7f12195fc5be6d72e5fe6a6e09d
Anchia Villegas <y.anchia.villegas@accenture.com>
2019-04-10 10:47:11 -0600

    polynomial regression on R, salary predictor

--- Part 2 - Regression/Section 6 - Polynomial Regression/my_solution.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/my_solution.R
@@ -1,16 +1,36 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Position_Salaries.csv' )
+dataset = dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# Fitting the Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+dataset$Level5 = dataset$Level^5
+dataset$Level6 = dataset$Level^6
+poly_reg = lm(formula = Salary ~ ., data = dataset)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+
+# predict a single point
+y_pred = predict(
+  poly_reg,
+  data.frame(
+    Level = 6.5,
+    Level2 = 6.5^2,
+    Level3 = 6.5^3,
+    Level4 = 6.5^4,
+    Level5 = 6.5^5,
+    Level6 = 6.5^6
+  )
+  )
+
+# visualizing the results -- install.packages('ggplot2')
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x=dataset$Level, y = dataset$Salary), colour = 'red')+
+  geom_line(aes(x=dataset$Level, y = predict(poly_reg, newdata = dataset)), colour = 'blue')
+
+
+
